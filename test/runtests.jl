41f16cf286ebc3b8ce4da732e46e9c11175b7925
Tim Wilson <twilsonco@gmail.com>
2023-08-15 17:56:06 -0600

    test: custom name

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,7 @@ using .SemanticRelease
 
 @testset "SemanticRelease.jl" begin
     @test greet() == "Hello, World!"
+    name = "Julia"
+    @test greet(name) == "Hello, $(name)!"
     # Write your tests here.
 end
